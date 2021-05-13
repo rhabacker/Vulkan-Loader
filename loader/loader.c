0b7b2cc028ca8e2610f24dd41261706bd6a6236f
Charles Giessen <charles@lunarg.com>
2021-05-12 18:20:37 -0600

    loader: Fix potential memory leak
    Also remove a redundant setting of an int to 0
    
    Change-Id: Ia099ad75e9da14aacf9874d75267716c12794305

--- loader/loader.c
+++ loader/loader.c
@@ -7833,7 +7833,7 @@ terminator_EnumerateInstanceExtensionProperties(const VkEnumerateInstanceExtensi
     }
 
 out:
-
+    loader_destroy_generic_list(NULL, (struct loader_generic_list *)&icd_tramp_list);
     loader_destroy_generic_list(NULL, (struct loader_generic_list *)&local_ext_list);
     loaderDeleteLayerListAndProperties(NULL, &instance_layers);
     return res;
@@ -8215,7 +8215,6 @@ out:
             }
             loader_instance_heap_free(inst, new_phys_dev_groups);
         }
-        total_count = 0;
     } else {
         // Free everything that didn't carry over to the new array of
         // physical device groups
